69e76b94b09980146ab4e04c552145010b854ac5
arthurgranito <arthurcursotecnico@gmail.com>
2025-05-09 08:46:44 -0300

    alteração no theme provider

--- src/components/theme-provider.jsx
+++ src/components/theme-provider.jsx
@@ -1,45 +1,15 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-// Definindo o contexto do ThemeProvider
 const ThemeProviderContext = createContext();
 
 export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState("system");
 
-  // Função para verificar o tema do sistema
-  const getSystemTheme = () => {
-    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-      ? "dark"
-      : "light";
-  };
-
-  // Efeito para aplicar o tema, baseado no estado ou no tema do sistema
   useEffect(() => {
     const root = document.documentElement;
-
-    // Definir o tema baseado no estado ou no sistema
-    if (theme === "system") {
-      const systemTheme = getSystemTheme();
-      root.classList.remove("dark", "light");
-      root.classList.add(systemTheme);
-    } else {
-      root.classList.remove("dark", "light");
-      root.classList.add(theme);
-    }
-
-    // Armazenar o tema no localStorage
-    localStorage.setItem("theme", theme);
+    root.classList.remove("dark", "light");
+    root.classList.add(theme);
   }, [theme]);
-
-  // Recupera o tema do localStorage, caso tenha sido salvo
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-      setTheme(savedTheme);
-    } else {
-      setTheme("system");
-    }
-  }, []);
 
   return (
     <ThemeProviderContext.Provider value={{ theme, setTheme }}>
@@ -48,7 +18,6 @@ export function ThemeProvider({ children }) {
   );
 }
 
-// Hook para acessar o contexto do tema
 export const useTheme = () => {
   const context = useContext(ThemeProviderContext);
   if (context === undefined) {
